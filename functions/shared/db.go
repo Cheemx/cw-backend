6c5b10cba9aebd93dc88933d81d5e2f91110d46b
Cheemx <chinmaymahan999@gmail.com>
2025-07-25 18:54:54 +0530

    tech blog model initiated and getAll lambda created

---
+++ functions/shared/db.go
@@ -0,0 +1,32 @@
+package shared
+
+import (
+	"sync"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var (
+	clientInstance *mongo.Client
+	clientOnce     *sync.Once
+)
+
+func GetClient(uri string) (*mongo.Client, error) {
+	var err error
+
+	clientOnce.Do(func() {
+		clientOptions := options.Client().ApplyURI(uri)
+		clientInstance, err = mongo.Connect(clientOptions)
+	})
+
+	return clientInstance, err
+}
+
+func GetCollection(name string, uri string) (*mongo.Collection, error) {
+	client, err := GetClient(uri)
+	if err != nil {
+		return nil, err
+	}
+	return client.Database("blogdb").Collection(name), nil
+}
