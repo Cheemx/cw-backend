55354862fcbaad53b02e17a397de069856f6ce85
Cheemx <chinmaymahajan999@gmail.com>
2025-07-25 18:55:59 +0530

    all models entered and some functions templates generated

---
+++ functions/models/wc.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type DailyBlog struct {
+	ID          bson.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title       string        `json:"title"`
+	Description string        `json:"description"`
+	Slug        string        `json:"slug"`
+	Content     string        `json:"content"`
+	Comments    []Comment     `json:"comments"`
+	CreatedAt   time.Time     `json:"createdAt"`
+}
